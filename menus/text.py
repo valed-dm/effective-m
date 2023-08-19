05e99af7146e35b21d3ec15f627f82bd84a00d00
valed-dm <dmvaled@gmail.com>
2023-08-19 17:25:12 +0300

    menus options added

---
+++ menus/text.py
@@ -0,0 +1,23 @@
+START_MENU = """
+    WELCOME TO PHONEBOOK!
+    Choose action from following:
+    1 - browse phonebook
+    2 - add new contact
+    3 - update record
+    4 - search contact
+    5 - quit
+    """
+
+ADD_MENU = """
+Choose action from following:
+1 - confirm data and save
+2 - repeat entry
+3 - quit without save
+"""
+
+SEARCH_MENU = """
+Choose action from following:
+1 - start search
+2 - repeat entry
+3 - quit
+"""
