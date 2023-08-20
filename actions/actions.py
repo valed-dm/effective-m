f38e059267b6b067d08ec8fc879b7b4fc163a24a
valed-dm <dmvaled@gmail.com>
2023-08-21 01:12:09 +0300

    multiple search facilities added

--- actions/actions.py
+++ actions/actions.py
@@ -1,25 +1,48 @@
 import pandas as pd
 
 
-def mask_standard(df, column, value):
-    mask = df[column] == value
-    return df[mask]
+def mask_standard(df, val_dict):
+    masks = []
+    for key, value in val_dict.items():
+        mask = df[key] == value[0]
+        masks.append(mask)
+    aggregate_mask = masks[0]
+    for mask in masks[1:]:
+        aggregate_mask = aggregate_mask & mask
+    return df[aggregate_mask]
 
 
-def search(column, value):
-    df = get_df()
-    res = mask_standard(df, column, value)
-    idx = res.index
-    print(res, idx)
-
-
-def sort():
-    df = pd.read_csv("phone_book.csv")
-    sorted_df = df.sort_values(by=["fname"])
-    sorted_df.to_csv("phone_book.csv", index=False)
+def mask_multiple(df, val_dict):
+    # val_dict = {"c_1": [v_1, v_2, ...], "c_2": [v_1, v_2, ...], ...}
+    dfs = []
+    for key, value in val_dict.items():
+        mask = df[key].isin(value)
+        dfs.append(df[mask])
+    return dfs
 
 
 def get_df():
     data = pd.read_csv("phone_book.csv")
     df = pd.DataFrame(data)
     return df
+
+
+def search(val_dict):
+    df = get_df()
+    res = mask_standard(df, val_dict)
+    print(res)
+    return res
+
+
+def search_multiple(data):
+    df = get_df()
+    dfs = mask_multiple(df, data)
+    for res in dfs:
+        print(res)
+    return dfs
+
+
+def sort():
+    df = pd.read_csv("phone_book.csv")
+    sorted_df = df.sort_values(by=["fname"])
+    sorted_df.to_csv("phone_book.csv", index=False)
--- inputs/inputs.py
+++ inputs/inputs.py
@@ -51,18 +51,21 @@ def user_input():
             business_phone,
             cellular_phone,
         )
+
         is_valid = True
 
     return user_data
 
 
 def search_input():
-    is_valid = False
-    search_data = ()
+    search_data = {}
 
-    while not is_valid:
+    while True:
+        print("start input; to exit enter 'stop'")
         column = input("enter column name:_")
-        if not column:
+        if column == "stop":
+            break
+        elif not column:
             print("please, enter column name")
             continue
         value = input("enter search value:_")
@@ -70,7 +73,60 @@ def search_input():
             print("please, enter search value")
             continue
 
-        search_data = column, value
-        is_valid = True
+        search_data[column] = (value,)
 
     return search_data
+
+
+def search_multiple_input():
+    search_data = {}
+
+    while True:
+        values = []
+        print("start input, to exit enter 'stop'")
+        column = input("enter column name:_")
+        if column == "stop":
+            break
+        elif not column:
+            print("please, enter column name")
+            continue
+
+        print("enter search values, to exit enter 'stop'")
+        while True:
+            value = input("enter next search value:_")
+            if value == "stop":
+                break
+            if not value:
+                print("please, enter next search value")
+                continue
+            values.append(value)
+
+        search_data[column] = values
+
+    return search_data
+
+
+def update_input():
+    is_valid = False
+    update_data = {}
+
+    while not is_valid:
+        row = input("enter row>")
+        if not row:
+            print("please, enter row")
+            continue
+        column = input("enter column name>")
+        if not column:
+            print("please, enter column name")
+            continue
+        new_value = input("enter new value>")
+        if not new_value:
+            print("please, enter new value")
+            continue
+
+        update_data[row] = column, new_value
+        print(update_data)
+
+        is_valid = True
+
+    return update_data
