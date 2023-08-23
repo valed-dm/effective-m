46917d2743a55e4898d668656ba917454f63f79b
valed-dm <dmvaled@gmail.com>
2023-08-23 12:20:05 +0300

    crud.py refactored

--- actions/crud.py
+++ actions/crud.py
@@ -1,17 +1,24 @@
+"""CRUD for .csv as database"""
+
 import csv
 
 import pandas as pd
 
-from actions.actions import sort, get_df
+from .actions import sort, get_df
 
 
-def create(fds, mode):
-    with open("phone_book.csv", mode, newline="") as f:
+def create(data):
+    """Creates row in .csv file with CSV File API usage"""
+
+    with open("phone_book.csv", mode="a", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
-        writer.writerow(fds)
+        # data - fields array [field_1, field_2, ...]
+        writer.writerow(data)
 
 
 def read():
+    """Reads .csv by pages. Page size = chunksize."""
+
     sort()
     csv_data = pd.read_csv("phone_book.csv", chunksize=50, iterator=True)
     while True:
@@ -23,14 +30,22 @@ def read():
 
 
 def update(update_data):
+    """Updates row column data stored in .csv"""
+
     df = get_df()
     str_row = list(update_data)[0]
     column = update_data[str_row][0]
     new_value = update_data[str_row][1]
     df.loc[int(str_row), [column]] = [new_value]
-    print(f"column name: {column}\nrow: {str_row}\nnew data: {new_value}")
+    print("update success!")
+    print(f"column name: {column} row: {str_row} new data: {new_value}")
     df.to_csv("phone_book.csv", index=False)
 
 
-def delete():
-    pass
+def delete(row):
+    """Deletes row from .csv using row number"""
+
+    df = get_df()
+    df.drop([int(row)], inplace=True)
+    print(f"row {row} successfully deleted!")
+    df.to_csv("phone_book.csv", index=False)
