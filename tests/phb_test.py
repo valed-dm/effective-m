883877227224dd7d9c46c3d5c116c668682efc4f
valed-dm <dmvaled@gmail.com>
2023-08-23 17:00:27 +0300

    add new record test added

--- tests/phb_test.py
+++ tests/phb_test.py
@@ -15,7 +15,7 @@ class TestPhoneBook(unittest.TestCase):
     def tearDown(self):
         os.remove(self.path)
 
-    def test_csv_create(self):
+    def test_csv(self):
         self.assertEqual(True, os.path.exists(self.path))
 
     def test_csv_headers(self):
@@ -25,6 +25,24 @@ class TestPhoneBook(unittest.TestCase):
             for field in fields:
                 headers.remove(field)
             self.assertEqual(0, len(headers))
+
+    def test_csv_create(self):
+        fds = [
+            "Paul",
+            "Smith",
+            "Batkovich",
+            "COMPANY",
+            "+1 (123) 456 7890",
+            "+2 (123) 456 7890",
+        ]
+        create(data=fds, p=self.path)
+        with open(self.path, "rt") as f:
+            reader = csv.reader(f)
+            _ = next(reader)
+            new_record = next(reader)
+            for field in fds:
+                new_record.remove(field)
+            self.assertEqual(0, len(new_record))
 
 
 if __name__ == "__main__":
