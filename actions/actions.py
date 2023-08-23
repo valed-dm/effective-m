4afb0bf51c9522c72c6049a46ed7bf9149384b84
valed-dm <dmvaled@gmail.com>
2023-08-23 12:21:22 +0300

    actions.py refactored

--- actions/actions.py
+++ actions/actions.py
@@ -1,48 +1,48 @@
+"""Gets, sorts, requests .csv file as database"""
+
 import pandas as pd
 
 
-def mask_standard(df, val_dict):
+def get_df():
+    """Creates dataframe from .csv"""
+
+    data = pd.read_csv("phone_book.csv")
+    df = pd.DataFrame(data)
+    return df
+
+
+def sort():
+    """Sorts and save sorted data to .csv"""
+
+    df = pd.read_csv("phone_book.csv")
+    sorted_df = df.sort_values(by=["fname"])
+    sorted_df.to_csv("phone_book.csv", index=False)
+
+
+def row_result(val_dict):
+    """Executes single row search with multiple columns data this row contains"""
+
+    df = get_df()
     masks = []
     for key, value in val_dict.items():
         mask = df[key] == value[0]
         masks.append(mask)
     aggregate_mask = masks[0]
     for mask in masks[1:]:
+        # aggregate_mask contains any given column data for row to search
         aggregate_mask = aggregate_mask & mask
-    return df[aggregate_mask]
-
-
-def mask_multiple(df, val_dict):
-    # val_dict = {"c_1": [v_1, v_2, ...], "c_2": [v_1, v_2, ...], ...}
-    dfs = []
-    for key, value in val_dict.items():
-        mask = df[key].isin(value)
-        dfs.append(df[mask])
-    return dfs
-
-
-def get_df():
-    data = pd.read_csv("phone_book.csv")
-    df = pd.DataFrame(data)
-    return df
-
-
-def search(val_dict):
-    df = get_df()
-    res = mask_standard(df, val_dict)
+    res = df[aggregate_mask]
     print(res)
     return res
 
 
-def search_multiple(data):
+def column_result(val_dict):
+    """Executes multiple row search with one or many column's values"""
+
     df = get_df()
-    dfs = mask_multiple(df, data)
-    for res in dfs:
-        print(res)
-    return dfs
-
-
-def sort():
-    df = pd.read_csv("phone_book.csv")
-    sorted_df = df.sort_values(by=["fname"])
-    sorted_df.to_csv("phone_book.csv", index=False)
+    key = list(val_dict)[0]
+    # .isin() considers multiple column values rows contain
+    mask = df[key].isin(val_dict[key])
+    res = df[mask]
+    print(res)
+    return res
