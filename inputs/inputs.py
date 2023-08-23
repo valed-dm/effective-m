241ffebbee213aae4134c3b86ec6375913f9689b
valed-dm <dmvaled@gmail.com>
2023-08-23 12:18:07 +0300

    inputs.py refactored

--- inputs/inputs.py
+++ inputs/inputs.py
@@ -1,132 +1,95 @@
+"""Provides user input timing and prompt functions"""
+
 import select
 import sys
 
+from .input import input_charfield, input_phonefield
+
 
 def timed_input(menu, delay):
+    """Provides app's certain menu's showtime duration"""
+
     print(menu)
     print(f"You have {delay} seconds to answer!\nEnter your choice: ")
     a, b, c = select.select([sys.stdin], [], [], delay)
     if a:
         return sys.stdin.readline().strip()
-    else:
-        print("Your time got over")
-        sys.exit(0)
+    print("Your time expired. Make another attempt.")
+    return sys.exit(0)
 
 
 def user_input():
-    is_valid = False
-    user_data = ()
+    """Asks for data to create a new record"""
 
-    while not is_valid:
-        first_name = input("enter your first name:_")
-        if not first_name:
-            print("please, enter first name")
-            continue
-        last_name = input("enter your last name:_")
-        if not last_name:
-            print("please, enter last name")
-            continue
-        middle_name = input("enter your middle name:_")
-        if not middle_name:
-            print("please, enter middle name")
-            continue
-        company = input("enter company name:_")
-        if not company:
-            print("please, enter company name")
-            continue
-        business_phone = input("enter business phone:_")
-        if not business_phone:
-            print("please, enter business phone")
-            continue
-        cellular_phone = input("enter cellular phone:_")
-        if not cellular_phone:
-            print("please, enter cellular phone")
-            continue
-
-        user_data = (
-            first_name,
-            last_name,
-            middle_name,
-            company,
-            business_phone,
-            cellular_phone,
-        )
-
-        is_valid = True
-
+    first_name = input_charfield("first_name")
+    last_name = input_charfield("last_name")
+    middle_name = input_charfield("middle_name")
+    company = input_charfield("company")
+    business_phone = input_phonefield("business_phone")
+    cellular_phone = input_phonefield("cellular_phone")
+    user_data = (
+        first_name,
+        last_name,
+        middle_name,
+        company,
+        business_phone,
+        cellular_phone,
+    )
     return user_data
 
 
-def search_input():
-    search_data = {}
-
-    while True:
-        print("start input; to exit enter 'stop'")
-        column = input("enter column name:_")
-        if column == "stop":
-            break
-        elif not column:
-            print("please, enter column name")
-            continue
-        value = input("enter search value:_")
-        if not value:
-            print("please, enter search value")
-            continue
-
-        search_data[column] = (value,)
-
-    return search_data
-
-
-def search_multiple_input():
-    search_data = {}
-
-    while True:
-        values = []
-        print("start input, to exit enter 'stop'")
-        column = input("enter column name:_")
-        if column == "stop":
-            break
-        elif not column:
-            print("please, enter column name")
-            continue
-
-        print("enter search values, to exit enter 'stop'")
-        while True:
-            value = input("enter next search value:_")
-            if value == "stop":
-                break
-            if not value:
-                print("please, enter next search value")
-                continue
-            values.append(value)
-
-        search_data[column] = values
-
-    return search_data
-
-
 def update_input():
-    is_valid = False
+    """Prompts and waits for row number, column name, new value to be entered"""
+
     update_data = {}
-
-    while not is_valid:
-        row = input("enter row>")
-        if not row:
-            print("please, enter row")
-            continue
-        column = input("enter column name>")
-        if not column:
-            print("please, enter column name")
-            continue
-        new_value = input("enter new value>")
-        if not new_value:
-            print("please, enter new value")
-            continue
-
+    while True:
+        row = input_charfield("row")
+        column = input_charfield("column")
+        new_value = input_charfield("new value")
         update_data[row] = column, new_value
         print(update_data)
-
-        is_valid = True
-
+        break
     return update_data
+
+
+def delete_input():
+    """Awaits user to enter row number to be deleted"""
+
+    row = input_charfield("row")
+    return row
+
+
+def search_single_row():
+    """Executes search 'one row - multiple columns values match'"""
+
+    search_data = {}
+    print("one row - many columns")
+    while True:
+        print("to exit enter 'stop'")
+        column = input_charfield("column")
+        if column == "stop":
+            break
+        value = input_charfield("value")
+        search_data[column] = (value,)
+    return search_data
+
+
+def search_multiple_rows():
+    """Executes search one column - multiple rows values match"""
+
+    search_data = {}
+    print("one column - many rows")
+    while True:
+        try:
+            list(search_data)[0]
+        except IndexError:
+            column = input_charfield("column")
+            search_data[column] = ()
+        print("to exit enter 'stop'")
+        while True:
+            value = input_charfield("value")
+            if value == "stop":
+                break
+            column = list(search_data)[0]
+            search_data[column] = search_data[column] + (value,)
+        return search_data
