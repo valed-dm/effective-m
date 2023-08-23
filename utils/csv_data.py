323f9d638b63651e97cd940e8e54885863760105
valed-dm <dmvaled@gmail.com>
2023-08-24 00:07:18 +0300

    сsv_data.py got new test data

--- utils/csv_data.py
+++ utils/csv_data.py
@@ -1,4 +1,41 @@
-"""Some .csv params storage"""
+"""Some .csv needed data storage"""
 
 path = "csv/phone_book.csv"
 fields = ["fname", "lname", "mname", "company", "bphone", "cphone"]
+fds = [
+    "Paul",
+    "Smith",
+    "Batkovich",
+    "COMPANY",
+    "+1 (123) 456 7890",
+    "+2 (123) 456 7890",
+]
+updated = [
+    "Paul",
+    "Smith",
+    "Batkovich",
+    "EFFECTIVE",
+    "+1 (123) 456 7890",
+    "+2 (123) 456 7890",
+]
+company_low = [
+    "Paul",
+    "Smith",
+    "Batk",
+    "company",
+    "+1 (123) 456 7890",
+    "+2 (123) 456 7890",
+]
+effective_low = [
+    "Paul",
+    "Smith",
+    "Batk",
+    "effective",
+    "+1 (123) 456 7890",
+    "+2 (123) 456 7890",
+]
+val_dict_1 = {
+    "fname": "Paul",
+    "mname": "Batk",
+    "company": "effective",
+}
