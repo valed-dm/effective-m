d5caba39d11af7cfbe43c27c581987ae45dca426
valed-dm <dmvaled@gmail.com>
2024-02-12 15:54:18 +0300

    timed_input is annotated

--- inputs/inputs.py
+++ inputs/inputs.py
@@ -6,14 +6,19 @@ import sys
 from .input import input_charfield, input_phonefield
 
 
-def timed_input(menu, delay):
-    """Provides app's certain menu's showtime duration"""
-
+def timed_input(menu: str, delay: int) -> str | None:
+    """Defines menu showtime duration
+    Args:
+        menu: Menu string
+        delay: Menu showtime in seconds
+    """
     print(menu)
     print(f"You have {delay} seconds to answer!\nEnter your choice: ")
+
     a, b, c = select.select([sys.stdin], [], [], delay)
     if a:
         return sys.stdin.readline().strip()
+
     print("Your time expired. Make another attempt.")
     sys.exit(0)
 
