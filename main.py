53cccf3fee60943b43afd8e5122724950a10f4e6
valed-dm <dmvaled@gmail.com>
2024-02-14 02:23:35 +0300

    timed_menu, main_menu_agent f names upgraded

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-"""Application entrypoint"""
+"""Phonebook entrypoint"""
 
 import os
 import sys
 
 from actions.crud import create
-from choices.menus import main_menu
+from choices.menus import main_menu_agent
 from choices.text import START_MENU
 from custom_exc.app_exit import AppExitError
-from inputs.inputs import timed_input
+from inputs.inputs import timed_menu
 from utils.csv_data import path
 
 
@@ -23,26 +23,29 @@ def main() -> None:
     7 - quit
     """
 
-    opt = timed_input(menu=START_MENU, delay=30)
-    main_menu(opt)
+    opt = timed_menu(menu=START_MENU, delay=30)
+    main_menu_agent(opt)
 
 
 if __name__ == "__main__":
+    # creates .csv storage when not found on utils.csv_data path
     if not os.path.exists(path):
         create()
 
-    # automatically restarts app and processes exceptions
+    # restarts or exits from phonebook app
     while True:
         try:
             main()
         except (StopIteration, KeyboardInterrupt):
+            # when raised makes user redirected to main menu;
             pass
         except KeyError:
-            # no search match found
+            # no search match found;
+            # makes user redirected to main menu for a new attempt;
             print("Request returns no data. Try other search.")
         except AppExitError:
-            # executes sys.exit for all menus exit point
-            print("phbook operation is terminated by user")
+            # executes sys.exit for all menus exit points;
+            print("phonebook operation is terminated by user")
             sys.exit(0)
         else:
             break
