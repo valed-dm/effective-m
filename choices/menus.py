cdde7d451150fcb4cd22f2873c61f8eccca08ff4
valed-dm <dmvaled@gmail.com>
2024-02-14 02:10:26 +0300

    menus.py functions names refactored
    'interface' added to functions names to clarify theirs roles
    sub_menu annotation got generic types from choices.gtypes
    menu_exec merged with sub_menu
    some comments added

--- choices/menus.py
+++ choices/menus.py
@@ -1,15 +1,15 @@
-"""Contains app's user interface menus"""
-from typing import Callable, Dict, Tuple, Union, List
+"""Contains phonebook user interface menus"""
 
-from pandas import DataFrame
+from typing import Callable
 
 from actions.actions import row_result, column_result
 from actions.crud import create, read, update, delete
+from choices.gtypes import handler_type, data_type
 from choices.text import SUB_MENU
 from custom_exc.app_exit import AppExitError
 from inputs.inputs import (
     user_input,
-    timed_input,
+    timed_menu,
     search_single_row,
     search_multiple_rows,
     update_input,
@@ -17,53 +17,43 @@ from inputs.inputs import (
 )
 
 
-def main_menu(opt: str) -> Callable[[], None] | AppExitError:
-    """Main menu
+def main_menu_agent(opt: str) -> None:
+    """Main menu agent function
+    Routes CRUD or SEARCH functions depending on user choice
     Args:
         opt: User choice string value
-    Returns:
-        CRUD or SEARCH functions depending on user choice
     Raises:
-        AppExitError: Custom exception is raised to quit app
+        AppExitError: Custom exception is raised to quit phonebook
     """
 
     options = {
         "1": read,
-        "2": add_row,
-        "3": update_row,
-        "4": delete_row,
-        "5": search_row,
-        "6": search_column,
+        "2": add_row_interface,
+        "3": update_row_interface,
+        "4": delete_row_interface,
+        "5": search_row_interface,
+        "6": search_column_interface,
     }
-
     if opt == "7":
         raise AppExitError
-
-    return options[opt]()
+    options[opt]()
 
 
 def sub_menu(
-        handler: Union[
-            Callable[[List[str], str], None],  # create
-            Callable[[Dict[str, Tuple[str, str]], str], None],  # update
-            Callable[[str, str], None],  # delete
-            Callable[[Dict[str, str], str], DataFrame],  # row_result
-            Callable[[Dict[str, Tuple[str]], str], DataFrame],  # column_result
-        ],
-        repeater: Callable[[], None],
+        handler: handler_type,
+        user_interface: Callable[[], None],
         delay: int,
-        data: Union[
-            None,
-            str,
-            Tuple[str],
-            Dict[str, str],
-            Dict[str, tuple[str, str]],
-        ] = None
+        data: data_type = None
 ) -> None:
-    """Subsequent menu
+    """Launches subsequent menu
+    SUB MENU
+    1 - execute
+    2 - repeat entry
+    3 - MAIN MENU
+    4 - quit
     Args:
         handler: CRUD or SEARCH functions depending on user choice
-        repeater: Repeats chosen entry operation anew to correct inputted data
+        user_interface: Repeats chosen entry operation anew to correct inputted data
         delay: Menu showtime in seconds
         data:
             None
@@ -71,37 +61,35 @@ def sub_menu(
             user_data: Tuple[str] - (str, str, str, str, str, str) new row data to insert
             val_dict: Dict[str, str]  - {column_1: value_1, column_2: value_2 ...} single row search by multiple values
             update_data: Dict[str, tuple[str, str]]  - {row_number: (column, value)} to update the selected field value
+    Raises:
+        StopIteration: Exception is raised to get to phonebook main menu
+        AppExitError: Custom exception is raised to quit app
     """
 
-    opt = timed_input(menu=SUB_MENU, delay=delay)
-    exec_menu(opt, data, handler, repeater)
-
-
-def exec_menu(opt, data, handler, repeater):
-    """Executes app menus"""
-
+    opt = timed_menu(menu=SUB_MENU, delay=delay)
     if opt == "1":
         handler(data)
-    elif opt == "2":
-        repeater()
-    elif opt == "3":
+    if opt == "2":
+        user_interface()
+    if opt == "3":
         raise StopIteration
-    elif opt == "4":
+    if opt == "4":
         raise AppExitError
 
 
-def add_row():
+def add_row_interface():
     """Guides user through add row task"""
 
     user_data = user_input()
     print("Check your input")
     print(user_data)
-    sub_menu(handler=create, repeater=add_row, delay=60, data=user_data)
+    sub_menu(handler=create, user_interface=add_row_interface, delay=60, data=user_data)
     print("your data successfully saved to phonebook")
+    # redirects to main menu
     raise StopIteration
 
 
-def update_row():
+def update_row_interface():
     """Guides user through update row task"""
 
     # user is to define needed row number
@@ -110,39 +98,47 @@ def update_row():
     print("Check your search data")
     for key, val in val_dict.items():
         print(key, val)
-    sub_menu(handler=row_result, repeater=search_row, delay=60, data=val_dict)
+    sub_menu(handler=row_result, user_interface=search_row_interface, delay=60, data=val_dict)
     # user is to enter row number, column name, new column value
     print("===enter update data=====================")
     update_data = update_input()
-    sub_menu(handler=update, repeater=update_row, delay=60, data=update_data)
+    sub_menu(handler=update, user_interface=update_row_interface, delay=60, data=update_data)
+
+    # redirects to main menu
     raise StopIteration
 
 
-def delete_row():
+def delete_row_interface():
     """Guides user through delete row task"""
 
     print("Enter row to delete")
     row = delete_input()
-    sub_menu(handler=delete, repeater=delete_row, delay=60, data=row)
+    sub_menu(handler=delete, user_interface=delete_row_interface, delay=60, data=row)
+
+    # redirects to main menu
     raise StopIteration
 
 
-def search_row():
+def search_row_interface():
     """Guides user through find single row task"""
 
     val_dict = search_single_row()
     print("Check your search data")
     for key, val in val_dict.items():
         print(key, val)
-    sub_menu(handler=row_result, repeater=search_row, delay=60, data=val_dict)
+    sub_menu(handler=row_result, user_interface=search_row_interface, delay=60, data=val_dict)
+
+    # redirects to main menu
     raise StopIteration
 
 
-def search_column():
+def search_column_interface():
     """Guides user through find multiple rows task"""
 
     val_dict = search_multiple_rows()
     print("Check your search data")
     print(val_dict)
-    sub_menu(handler=column_result, repeater=search_column, delay=60, data=val_dict)
+    sub_menu(handler=column_result, user_interface=search_column_interface, delay=60, data=val_dict)
+
+    # redirects to main menu
     raise StopIteration
