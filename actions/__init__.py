5bcbc6401ce8a797e45330de0ec00c23fa61b7a5
valed-dm <dmvaled@gmail.com>
2024-02-15 01:55:48 +0300

    actions imports follows app layout redesign

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,4 +1,4 @@
 """Actions imports optimized"""
 
-from .actions import row_result, column_result
+from .actions import row_result, column_result, search_single_row, search_multiple_rows
 from .crud import create, update, delete
