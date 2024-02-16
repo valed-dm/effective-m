154d22c3c5868123a2fb8979c40111be280bc091
valed-dm <dmvaled@gmail.com>
2024-02-16 11:52:23 +0300

    sub_menu_decorator got handler_execute_switch
    handler_execute_switch=True when option EXECUTE is chosen in SUB MENU

--- menus/sub_menu_decorator.py
+++ menus/sub_menu_decorator.py
@@ -1,7 +1,6 @@
 """Subsequent Menu Decorator"""
 
 from functools import wraps
-from typing import Callable
 
 from menus.gtypes import handler_type
 from menus.sub_menu import sub_menu
@@ -13,11 +12,15 @@ def sub_menu_decorator(**kwargs: handler_type | int):
         **kwargs: CRUD or SEARCH functions; Menu showtime in seconds.
     """
 
-    def inner_decorator(func: Callable[[], None | bool]) -> Callable[[], None | bool]:
+    def inner_decorator(func):
         @wraps(func)
         def wrapper():
             data = func()
-            sub_menu(**kwargs, user_interface=func, data=data)
+            handler = kwargs["handler"]
+            # handler_execute_switch=True when option EXECUTE is chosen in SUB MENU
+            handler_execute_switch = sub_menu(user_interface=func, delay=kwargs["delay"])
+            if handler_execute_switch:
+                handler(data)
             # Redirects user to MAIN MENU after requested task finished
             raise StopIteration
 
