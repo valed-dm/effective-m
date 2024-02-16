c590fca1c218c6f48aafa1d34c58a292fc6d9594
valed-dm <dmvaled@gmail.com>
2024-02-16 05:36:19 +0300

    gtypes.py got row_type

--- menus/gtypes.py
+++ menus/gtypes.py
@@ -1,21 +1,21 @@
 """Generic types used in functions annotations"""
 
-from typing import Callable, Dict, List, Tuple, Union
+from typing import Callable, Dict, Tuple, Union
 
 from pandas import DataFrame
 
+row_type = Tuple[str, str, str, str, str, str,]
 data_type = Union[
-    None,
     str,
     Tuple[str],
     Dict[str, str],
-    Dict[str, tuple[str, str]],
+    Dict[str, Tuple[str, str]],
+    row_type
 ]
-
 handler_type = Union[
-    Callable[[List[str], str], None],  # create
-    Callable[[Dict[str, Tuple[str, str]], str], None],  # update
-    Callable[[str, str], None],  # delete
-    Callable[[Dict[str, str], str], DataFrame],  # row_result
-    Callable[[Dict[str, Tuple[str]], str], DataFrame],  # column_result
+    Callable[[row_type], None],
+    Callable[[str, str], None],
+    Callable[[Dict[str, str], str], DataFrame],
+    Callable[[dict[str, tuple[str]], str], DataFrame],
+    Callable[[Dict[str, Tuple[str, str]], str], None]
 ]
